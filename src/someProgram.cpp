9bd1a6e77956c84e2ebd28292a3d552448cde9cd
roozbehsayadi <roozbehsayadi@gmail.com>
2019-04-02 14:53:20 +0430

    A cplusplus code added to repository, plus it's output.

---
+++ src/someProgram.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Test { 
+	double a = 0.341e5;
+};
+
+int main () { 
+
+	int a2_;
+	a2_ = 34593845;
+	
+	char c1 = 'b', c2 = '\0';
+
+	string s = "Te\nst";
+	//testing string highliting
+	
+	/*
+	This is a multi-line comment
+	*/
+	
+	return 0;
+
+}
+
